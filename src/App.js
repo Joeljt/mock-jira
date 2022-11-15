98c91d454d049045bcbd2547f1d20c01fe347292
Joseph_L <joseph_l@126.com>
2022-11-15 18:03:37 +0800

    feat: 添加 commit lint

--- src/App.js
+++ src/App.js
@@ -6,9 +6,12 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
+        {/*测试 git 提交*/}
+
         <a
           className="App-link"
           href="https://reactjs.org"
