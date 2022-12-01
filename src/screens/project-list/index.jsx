2e5944a884dde4cef1f994ad43a2d0f941d0a313
Joseph_L <joseph_l@126.com>
2022-12-01 12:58:50 +0800

    feat: add useDebounce custom hook

--- src/screens/project-list/index.jsx
+++ src/screens/project-list/index.jsx
@@ -3,7 +3,7 @@ import {SearchPanel} from "./search-panel";
 import {List} from "./list";
 import {useEffect, useState} from "react";
 import qs from "qs";
-import {cleanObject} from "../../utils";
+import {cleanObject, useDebounce, useMount} from "../../utils";
 
 // 利用 .env 来实现正式环境和测试环境的区分，webpack 打包的时候会直接使用 .env 的值
 // 这样就不用在代码中对 develop url 做硬编码，避免出错
@@ -14,6 +14,9 @@ export const ProjectListScreen = () => {
     name: '',
     personId: ''
   })
+
+  const debouncedParam = useDebounce(param, 1000)
+
   // 获取用户列表的数据和 setter
   const [users, setUsers] = useState([])
   const [list, setList] = useState([])
@@ -21,21 +24,21 @@ export const ProjectListScreen = () => {
   // 每次当 param 发生变化的时候，都重新请求接口，刷新列表
   // 使用 cleanObject 工具方法过滤掉空值
   useEffect(() => {
-    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(param))}`).then(async response => {
+    fetch(`${apiUrl}/projects?${qs.stringify(cleanObject(debouncedParam))}`).then(async response => {
       if (response.ok) {
         setList(await response.json())
       }
     })
-  }, [param])
+  }, [debouncedParam])
 
   // 初始化用户列表的数据，只初始化一次，因为这个数据不会发生变化
-  useEffect(() => {
+  useMount(() => {
     fetch(`${apiUrl}/users`).then(async response => {
       if (response.ok) {
         setUsers(await response.json())
       }
     })
-  }, [])
+  })
 
   return <div>
     <SearchPanel users={users} param={param} setParam={setParam}/>
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,4 +1,6 @@
 // 过滤掉 value 为 0 的情况，为 0 时不认为是 false，其余场景下才认为是 false
+import {useEffect, useState} from "react";
+
 const isFalsy = (value) => value === 0 ? false : !value;
 
 // 将空值从传入的 object 中清除，返回拷贝到新对象，不修改源数据
@@ -11,4 +13,23 @@ export const cleanObject = (object) => {
     }
   })
   return result
+}
+
+export const useMount = (callback) => {
+  useEffect(() => {
+    callback()
+  }, [])
+}
+
+export const useDebounce = (value, delay) => {
+  const [debouncedValue, setDebouncedValue] = useState(value)
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      setDebouncedValue(value)
+    }, delay)
+    return () => clearTimeout(timeout)
+  }, [value, delay])
+
+  return debouncedValue;
 }
